ebc8c676de4d166fca5148a35f4da570d3442190
Bloxyy <103850073+Bloxyy@users.noreply.github.com>
2022-04-16 21:02:16 +0300

    Update botConfig.js

--- botConfig.js
+++ botConfig.js
@@ -2,7 +2,7 @@ module.exports = {
     token: "OTE2NDY4OTk4OTMyMTU2NDM3.YaqmRA.lWuYyIpt-e8TvWb8Z4oJn8Qgi-4",
     prefix: "!",
     admins: [
-        "451474544255959071"
+        "869601791401156628"
 ],
     sayikanali: "countChannelID"
 };
